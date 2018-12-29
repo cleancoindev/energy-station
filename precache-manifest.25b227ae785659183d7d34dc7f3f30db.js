2fe108f55e0fa186e9aaef79e9f3832fda00356a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-29 08:47:31 +0000

    Deploy libotony/energy-station to github.com/libotony/energy-station.git:gh-pages

---
+++ precache-manifest.25b227ae785659183d7d34dc7f3f30db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "137f6dd5015950cecc66",
+    "url": "/energy-station/css/app.a2ebf2c0.css"
+  },
+  {
+    "revision": "137f6dd5015950cecc66",
+    "url": "/energy-station/js/app.95f938cb.js"
+  },
+  {
+    "revision": "a02725957e10d6586bd5",
+    "url": "/energy-station/css/chunk-vendors.29282930.css"
+  },
+  {
+    "revision": "a02725957e10d6586bd5",
+    "url": "/energy-station/js/chunk-vendors.a26a4b08.js"
+  },
+  {
+    "revision": "8ce0eb849cb07beaf98ff0d943c47c95",
+    "url": "/energy-station/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/energy-station/robots.txt"
+  }
+];
